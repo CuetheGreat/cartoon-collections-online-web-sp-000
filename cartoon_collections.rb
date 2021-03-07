c589461d87636115991297ea60a46a25c86cd92a
Clayon Stewart <c.stewart89@gmail.com>
2021-03-07 19:07:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(collection)
+  if collection.length != 0
+    collection.each_with_index { |dwarf,index| puts "#{index + 1}. #{dwarf}" }
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(collection)
+  new_collection =[]
+  collection.each { |element| new_collection << "#{element.upcase}!"}
+  new_collection
+end
+ 
+def long_planeteer_calls(collection)
+  new_collection = collection.collect do |item|
+    item.length <= 4
+  end
+  new_collection.include?(false)
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(collection)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
